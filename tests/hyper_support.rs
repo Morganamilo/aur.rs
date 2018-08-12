b62c1e38044f334edf2fed33f2f383797e230f4c
Zeyla Hellyer <hi@zeyla.me>
2018-08-12 11:22:19 -0700

    Update dev dependency tokio-core to tokio

--- tests/hyper_support.rs
+++ tests/hyper_support.rs
@@ -2,14 +2,13 @@ extern crate aur;
 extern crate futures;
 extern crate hyper;
 extern crate hyper_tls;
-extern crate tokio_core;
+extern crate tokio;
 
 use aur::AurHyperRequester;
 use futures::Future;
 use hyper::client::HttpConnector;
 use hyper::{Body, Client};
 use hyper_tls::HttpsConnector;
-use tokio_core::reactor::Core;
 
 #[inline]
 fn client() -> Client<HttpsConnector<HttpConnector>, Body> {
@@ -18,26 +17,22 @@ fn client() -> Client<HttpsConnector<HttpConnector>, Body> {
 
 #[test]
 fn test_info() {
-	let mut core = Core::new().unwrap();
-	let client = client();
-
-	let done = client.aur_info(&["rust-nightly"]).map(|search| {
+	let done = client().aur_info(&["rust-nightly"]).map(|search| {
         assert_eq!(search.result_count, 1);
-    }).map_err(|_| ());
+    }).map_err(|why| {
+        panic!("Err testing info: {:?}", why);
+    });
 
-	core.run(done).expect("core err");
+	tokio::run(done);
 }
 
 #[test]
 fn test_search() {
-	let mut core = Core::new().unwrap();
-	let client = client();
-
-	let done = client.aur_search(Some("rust"), None).map(|search| {
+	let done = client().aur_search(Some("rust"), None).map(|search| {
         assert!(search.result_count >= 2);
     }).map_err(|why| {
         panic!("Err searching: {:?}", why);
     });
 
-	core.run(done).expect("core err");
+	tokio::run(done);
 }
